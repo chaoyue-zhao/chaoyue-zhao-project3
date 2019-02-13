ca41c5cbb455454609e303c2a6e89b9d008fa9af
Chaoyue <chaoyue.zhao.2011@gmail.com>
2019-02-12 22:49:32 -0500

    major milestone ✌🏻 -- number of div generated is now based on user's input

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
-
-const mondrianApp = {}; 
-//create a variable that select the mondrian in html
+/*********************
+ CACHE DA SELECTORS
+ *********************/
 const $mondrian = $(".mondrian");
-//create a variable that select the element inside the mondrian in html 
-// const $element = $(".element");
+const $element = $(".element");
+const $form = $(".user-control");
+const $number = $("input[type=range]").val();
+
+/*********************************
+ JUICY CODES TO GET DA APP GOING
+**********************************/
+const mondrianApp = {}; 
+
+//create a variable that store the data (list of elements) 
+mondrianApp.mondrian = [];
+
+//write a function to push objects (element) to the Mondrian array 
+mondrianApp.gatherUsersInput = function() {
+    for (i = 1; i < $number; i++) {
+        mondrianApp.mondrian.push({
+         //using the random number generated as width and height
+          width: mondrianApp.randomNum(5),
+          height: mondrianApp.randomNum(5)
+        });
+    }
+};
+
 //write a function that render to the DOM
-mondrianApp.renderHTML = function (horizontalSpan, verticalSpan) {
-    
-    $mondrian.append(`
-        <div class = "horizontal-span-${horizontalSpan} vertical-span-${verticalSpan} element">element</div>
-    `);
+mondrianApp.renderHTML = function () {
+
+    //call the function to retrieve information from user
+    mondrianApp.gatherUsersInput();
+
+    mondrianApp.mondrian.forEach(function(element){
+
+        $mondrian.append(`
+            <div class = "horizontal-span-${element.width} vertical-span-${element.height} element">element
+            </div>
+        `);
+    });
+
 };
 
 //write a function that generate a random number 
@@ -18,7 +47,12 @@ mondrianApp.randomNum = function (max) {
 }
 
 mondrianApp.init = function () {
-    mondrianApp.renderHTML(mondrianApp.randomNum(5), mondrianApp.randomNum(5));
+
+    $form.on("submit",function (event) {
+        event.preventDefault();
+
+        mondrianApp.renderHTML();
+    })
 };
 
 $(document).ready(function(){
