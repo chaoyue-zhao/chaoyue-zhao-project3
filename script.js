3384f340f23545748e29e2b5244c26c6be071727
Chaoyue <chaoyue.zhao.2011@gmail.com>
2019-02-14 07:32:43 -0500

    major major milestone: color🎨 feature is now working

--- script.js
+++ script.js
@@ -14,16 +14,23 @@ mondrianApp.gatherUsersInput = function() {
     mondrianApp.$color = [
         $("input[name=color-1]").val(),
         $("input[name=color-2]").val(),
-        $("input[name=color-3]").val()
+        $("input[name=color-3]").val(),
+        //off-white color
+        "#FAFAFA",
+        //off-black color
+        "#232323"
     ];
-    
+
     for (i = 1; i <= mondrianApp.$number; i++) {
       mondrianApp.mondrian.push({
         //using the random number generated as width and height
         width: mondrianApp.randomNum(5),
-        height: mondrianApp.randomNum(5)
+        height: mondrianApp.randomNum(5),
+        //randomly select a color off the color array
+        color: mondrianApp.$color[mondrianApp.randomNum(mondrianApp.$color.length-1)],
       });
     }
+    console.log (mondrianApp.mondrian);
 };
 
 //write a function that render to the DOM
@@ -34,14 +41,17 @@ mondrianApp.renderHTML = function () {
 
     //call the function to retrieve information from user
     mondrianApp.gatherUsersInput();
-    mondrianApp.mondrian.forEach(function(element){
 
-            mondrianApp.$mondrian.append(`
-            <div class = "horizontal-span-${
-              element.width
-            } vertical-span-${element.height} element">element
-            </div>
-        `);
+    mondrianApp.mondrian.forEach(function(element,index){
+        
+        index = mondrianApp.mondrian.indexOf(element);
+
+        //create element based on the width and height randomized (see css for defined span value)
+        mondrianApp.$mondrian.append(`
+        <div class = "horizontal-span-${element.width} vertical-span-${element.height} element element-${index}"></div>`);
+
+        $(`.element-${index}`).css("background-color", `${element.color}`);
+        
     });
 };
 
@@ -63,7 +73,6 @@ mondrianApp.init = function () {
     CACHE DA SELECTORS
     *********************/
     mondrianApp.$mondrian = $(".mondrian");
-    mondrianApp.$element = $(".element");
     mondrianApp.$form = $(".user-control"); 
     
     mondrianApp.handleSubmit();
