821872cc88d941e3a89ca3d98994ab38d65d84cf
Chaoyue <chaoyue.zhao.2011@gmail.com>
2019-02-13 19:10:25 -0500

    empty state◻️ is finally working thanks to the helps of many people

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-/*********************
- CACHE DA SELECTORS
- *********************/
-const $mondrian = $(".mondrian");
-const $element = $(".element");
-const $form = $(".user-control");
-const $number = $("input[type=range]").val();
-
 /*********************************
  JUICY CODES TO GET DA APP GOING
 **********************************/
@@ -16,29 +8,41 @@ mondrianApp.mondrian = [];
 
 //write a function to push objects (element) to the Mondrian array 
 mondrianApp.gatherUsersInput = function() {
-    for (i = 1; i < $number; i++) {
-        mondrianApp.mondrian.push({
-         //using the random number generated as width and height
-          width: mondrianApp.randomNum(5),
-          height: mondrianApp.randomNum(5)
-        });
+
+    //get the user's input from the DOM
+    mondrianApp.$number = $("input[type=range]").val();
+    mondrianApp.$color = [
+        $("input[name=color-1]").val(),
+        $("input[name=color-2]").val(),
+        $("input[name=color-3]").val()
+    ];
+    
+    for (i = 1; i <= mondrianApp.$number; i++) {
+      mondrianApp.mondrian.push({
+        //using the random number generated as width and height
+        width: mondrianApp.randomNum(5),
+        height: mondrianApp.randomNum(5)
+      });
     }
 };
 
 //write a function that render to the DOM
 mondrianApp.renderHTML = function () {
+    //VERY VERY IMPORTANT TO DO BOTH: you want clear both the DOM and your array
+    mondrianApp.mondrian = [];
+    $(".mondrian").empty();
 
     //call the function to retrieve information from user
     mondrianApp.gatherUsersInput();
-
     mondrianApp.mondrian.forEach(function(element){
 
-        $mondrian.append(`
-            <div class = "horizontal-span-${element.width} vertical-span-${element.height} element">element
+            mondrianApp.$mondrian.append(`
+            <div class = "horizontal-span-${
+              element.width
+            } vertical-span-${element.height} element">element
             </div>
         `);
     });
-
 };
 
 //write a function that generate a random number 
@@ -46,13 +50,23 @@ mondrianApp.randomNum = function (max) {
     return Math.floor(Math.random() * max) + 1;
 }
 
-mondrianApp.init = function () {
-
-    $form.on("submit",function (event) {
+mondrianApp.handleSubmit = function () {
+    mondrianApp.$form.on("submit", function(event) {
         event.preventDefault();
-
         mondrianApp.renderHTML();
-    })
+    });
+}
+
+mondrianApp.init = function () {
+    
+    /*********************
+    CACHE DA SELECTORS
+    *********************/
+    mondrianApp.$mondrian = $(".mondrian");
+    mondrianApp.$element = $(".element");
+    mondrianApp.$form = $(".user-control"); 
+    
+    mondrianApp.handleSubmit();
 };
 
 $(document).ready(function(){
